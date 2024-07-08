a3b4471a1e55cd7fb12af684a302c7fe86cf4f34
Aixufey <jack_11728@msn.com>
2024-07-08 19:47:38 +0200

    fix errors and formatting

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,6 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import './ProductList.css'
+import { useDispatch } from 'react-redux';
+import { addItem } from './CreatSlice';
 function ProductList() {
+    const [addedToCart, setAddedToCart] = useState({});
+    const dispatch = useDispatch();
+
+    const handleAddToCart = (item) => {
+        dispatch(addItem(item));
+        setAddedToCart((prev) => ({
+            ...prev,
+            // K/V pair for product name and value as true when added to cart
+            [item.name]: true
+        }));
+    }
 
     const plantsArray = [
         {
@@ -246,15 +259,17 @@ function ProductList() {
                 </div>
                 <div style={styleObjUl}>
                     <div> <a href="#" style={styleA}>Plants</a></div>
-                    <div> <a href="#" style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                    <div> <a href="#" style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
                 </div>
             </div>
 
             <div className="product-grid">
                 {plantsArray.map((item, index) => (
-                    <>
-                        <h2>{item.category}</h2>
-                        <div className='product-list' key={index}>
+                    <div key={index} style={{
+                        textAlign: 'center',
+                    }}>
+                        <h1>{item.category}</h1>
+                        <div className='product-list'>
                             {item.plants.map((plant, index) => (
 
                                 <div className='product-card' key={index}>
@@ -262,13 +277,16 @@ function ProductList() {
                                     <img className='product-image' src={plant.image} alt={plant.name} />
                                     <p className='product-price'>{plant.cost}</p>
                                     <p>{plant.description}</p>
-                                    <button className='product-button'>
+                                    <button
+                                        className='product-button'
+                                        onClick={() => handleAddToCart(plant)}
+                                    >
                                         Add to Cart
                                     </button>
                                 </div>
                             ))}
                         </div>
-                    </>
+                    </div>
                 ))
 
                 }
